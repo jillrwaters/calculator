70cb5eba1fbf929a25da40e2a9b17aabb8a95cd3
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-09 21:52:42 -0500

    wrote out some logic in the text file and created new js file for clean slate

--- scripts.js
+++ scripts.js
@@ -1,28 +1,28 @@
 // constants
-const NUMBERS_DIV = document.querySelector(".numbers")
 const DISPLAY = document.querySelector(".display")
-
-const BUTTONS = [...document.querySelectorAll("button")]
-const ADD_BTN = document.querySelector("#add")
-const SUBTRACT_BTN = document.querySelector("#subtract")
-const MULTIPLY_BTN = document.querySelector("#multiply")
-const DIVIDE_BTN = document.querySelector("#divide")
-const EQUALS_BTN = document.querySelector("#equals")
-
-
-let operatorA
-let operatorB
-
-
+// operators
+let optA
+let optB
+// display value
 let dV = DISPLAY.textContent
+// operands
+let opdA
+let opdB
 
-let operandA
-let operandB
+// -------------------------------- NEW OBJECT - BELOW
+
+let info = {
+    opdA: "",
+    opdB: "",
+    optA: "",
+    optB: ","
+}
+
+// ------------------------------- ^ NEW OBJECT ^
 
 
 
-// basic math functions
-
+// -------------------- math functions 
 const add = (a, b) => {
     return a + b
 }
@@ -39,47 +39,53 @@ const divide = (a, b) => {
     return a / b
 }
 
+const isInt =(n) => {
+    return n % 1 === 0;
+ }
+
+const roundTwoDecimals = (num) => {
+    return Math.round((num + Number.EPSILON) * 100) / 100
+}
+// ------------------- ^ math functions ^
+
 const testResults = () => {
-    console.log(`operandA: ${operandA} | operandB: ${operandB} | operatorA: ${operatorA} | operatorB: ${operatorB} | dV: ${dV} | `)
+    console.log(`opdA: ${opdA} | opdB: ${opdB} | optA: ${optA} | optB: ${optB} | dV: ${dV} | `)
 }
 
 
-/
-// -------------------- replace this function with updateDisplay()
-// const showNumber = (num) => {
-//     if (DISPLAY.textContent.length < 11) {
-//         DISPLAY.textContent === "0" ? DISPLAY.textContent = `${num}` : DISPLAY.textContent += `${num}`;
-//     }
-// }
-// ------------------ ^ replace this function with updateDisplay() ^
 
-
-// ------------------------------------- change screen info
-const updateDisplay = (info) => {
-    // triggered inside another function after storing info or doing calculation
-    if (DISPLAY.textContent.length < 11) {
-        DISPLAY.textContent === "0" ? DISPLAY.textContent = `${info}` : DISPLAY.textContent += `${info}`;
+// ------------------------------------- change screen info - below -------------------------------------------------- keep
+// -------------------- replace function below with updateDisplay() ?
+const inputNum = (num) => {
+    if (DISPLAY.textContent.length < 11) { // if less than 11 chars, check following:
+        DISPLAY.textContent === "0" ? DISPLAY.textContent = `${num}` : DISPLAY.textContent += `${num}`;
     }
 }
+// ------------------ ^ replace function with updateDisplay() ? no. keep the same ^
+const updateDisplay = (info) => {
+    // triggered inside another function after storing info or doing calculation
+    DISPLAY.textContent = `${info}`
+}
 // ^ NEW FUNCTION ^
-
 const clearEverything = () => {
     dV = ""
     DISPLAY.textContent = "0"
-    operatorA = ""
-    operandA = ""
-    operandB = ""
+    optA = ""
+    opdA = ""
+    opdB = ""
 }
-// -------------------------------------- ^ change screen info ^
+// -------------------------------------- ^ change screen info ^ --------------------------------------------------- ^ keep ^
 
 
-// -------------------------------------- store operands 
+// -------------------------------------- store info - below
 
 const newOpd = (btn) => {
-    // triggered when any operand is pressed. stores last operand pressed
-    dV = DISPLAY.textContent
-    operatorA = btn
-    !operandA ? operandA = parseFloat(dV) : operandB = parseFloat(dV);
+    // triggered when [?]. stores last operand pressed
+    let final = '' // return value = newest operand stored ???
+    dV = DISPLAY.textContent // 
+    optA = btn
+    !opdA ? opdA = parseFloat(dV) : opdB = parseFloat(dV);
+    // the new operand is what and is stored where???
 }
 // ^ EDITING FUNCTION  ^
 
@@ -88,42 +94,34 @@ const newOpt = (btn) => {
 } 
 
 // ^ NEW FUNCTION ^
-// -------------------------------------- ^ store operands ^
-
-const isInt =(n) => {
-    return n % 1 === 0;
- }
-
-const roundTwoDecimals = (num) => {
-    return Math.round((num + Number.EPSILON) * 100) / 100
-}
+// -------------------------------------- ^ store info ^
 
 
 // --------------------------------------
 
 const operate = (op) => {
-    if (!operandB) {
-        operandB = parseFloat(DISPLAY.textContent)
+    if (!opdB) {
+        opdB = parseFloat(DISPLAY.textContent)
     }
 
 
     switch (op) {
         case "+":
-            DISPLAY.textContent = `${roundTwoDecimals(add(operandA, operandB))}`;
+            DISPLAY.textContent = `${roundTwoDecimals(add(opdA, opdB))}`;
             break;
         case "-":
-            DISPLAY.textContent = `${roundTwoDecimals(subtract(operandA, operandB))}`;
+            DISPLAY.textContent = `${roundTwoDecimals(subtract(opdA, opdB))}`;
             break;
         case "*":
-            DISPLAY.textContent = `${roundTwoDecimals(multiply(operandA, operandB))}`;
+            DISPLAY.textContent = `${roundTwoDecimals(multiply(opdA, opdB))}`;
             break;
         case "/":
-            DISPLAY.textContent = `${roundTwoDecimals(divide(operandA, operandB))}`;
+            DISPLAY.textContent = `${roundTwoDecimals(divide(opdA, opdB))}`;
             break;
     }
 testResults()
 
-    // display entire expression? DISPLAY.textContent = `${operandA} ${op} ${operandB} = ${answer}`
+    // display entire expression? DISPLAY.textContent = `${opdA} ${op} ${opdB} = ${answer}`
 }
 
 // -----------------------------------------
