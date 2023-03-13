eb276461d7a18656ad964bd9804316538b8f7532
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-13 14:56:19 -0400

    calculate() function works independently from calculator, trying to get correct result to show on screen instead of '0'

--- scripts2.js
+++ scripts2.js
@@ -3,11 +3,26 @@ let operands = []
 let operators = []
 
 const testResults = () => {
-    console.log(`operands: ${[operands]} | operators: ${[operators]}`)
+    console.table(`operands: ${[operands]} | operators: ${[operators]} | expression: ${{expression}}`)
 }
+
+const hasDecimal = () => {
+    return Array.from(SCREEN.textContent.toString()).includes(".")
+}
+
 const inputNum = (entry) => {
     let data = entry.textContent
-    if ((SCREEN.textContent).length < 11) SCREEN.textContent === "0" ? SCREEN.textContent = entry : SCREEN.textContent += entry
+    // if (SCREEN.classList.contains(".calculated")){
+    //     SCREEN.textContent = ""
+    // }
+    if (entry || (entry === "." && !hasDecimal())) {
+        if ((SCREEN.textContent).length < 11) SCREEN.textContent === "0" ? SCREEN.textContent = entry : SCREEN.textContent += entry
+
+    }
+
+    testResults()
+
+    // if operate hasn't already run
 }
 
 const updateScreen = (data) => {
@@ -35,32 +50,35 @@ const newOpd = () => {
 
 
 const newOpt = (data) => {
+    if (data !== "="){
     operators.push(data)
-
+    }
+    testResults()
 }
 let opdA
 let opdB
 
 let opt
+let expression = []
 
 const calculate = () => {
-    // newOpd()
+    let result = 0
     for (let i=0;i<operands.length;i++){
-        for (let t in operators){
-            opdA = operands[i]
-            operands.shift()
-            opdB = operands[i]
-            operands.shift()
-            opt = operators[i]
-            operators.shift()
-           
-        }
-    operate(opt, opdA, opdB)
-    } 
+    for (let k=0;k<operators.length;k++){
+        let previous = operands[0]
+        let next = operands[1]
+        let operator = operators[0]
+        
+        operate(operator, previous, next)
 
-    
-    console.log(operands, operators)
-    
+        operators = operators.slice(1)
+        operands = operands.slice(2)
+        operands.unshift(result)
+    }
+}
+    SCREEN.textContent = `${operands[0]}`
+   
+
 }
 
 
---
+++ trial-functions.js
@@ -0,0 +1,8 @@
+let operands = [12,7,5,3]
+let operators = ["+", "-", "*"]
+
+let calculate = () => {
+    
+}
+
+calculate()
