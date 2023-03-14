6efedf859129c92f49eb6d0ba1d6e26ceb1c18a2
jrw <72105852+jillrwaters@users.noreply.github.com>
2023-03-13 21:18:16 -0400

    can now do multiple operations in a row. 12+7-5*3 comes out to 42, but pressing 12+7 then trying to subtract from 19 after hitting equals returns wrong solution

---
+++ scripts3.js
@@ -0,0 +1,109 @@
+const SCREEN = document.querySelector(".screen")
+const NUMBERS = Array.from(document.querySelectorAll(".number"))
+// add event listeners to number buttons
+NUMBERS.forEach(function (num) {
+    num.addEventListener("click", function (e) {
+        if (SCREEN.textContent == 0) {
+            SCREEN.textContent = e.target.textContent
+        }
+        else {
+            SCREEN.textContent += `${e.target.textContent}`
+        }
+    })
+})
+// update the screen
+const updateScreen = (data) => {
+    SCREEN.textContent = `${data}`
+}
+
+// clear the screen
+const CLEAR = document.querySelector(".clear")
+CLEAR.addEventListener("click", function (e) {
+    SCREEN.textContent = `0`
+    operators = []
+    operands = []
+    e.target.classList.remove(".operated")
+    testing()
+})
+
+const OPERATORS = Array.from(document.querySelectorAll(".operator"))
+// initialize arrays for operands and operators
+let operands = []
+let operators = []
+// add event listeners to operators
+OPERATORS.forEach(function (op) {
+    op.addEventListener("click", function (e) {
+        // push whatever is on screen to operands
+        operands.push(SCREEN.textContent)
+        // push operator pressed to operators
+        operators.push(e.target.textContent)
+        updateScreen('0')
+        testing()
+
+
+    })
+})
+
+// -------testing function
+const testing = () => {
+    console.log(`operands: [${operands}] | operators: ${operators}`)
+}
+// -------
+
+const EQUALS = document.querySelector(".equals")
+// add event listener to equals button
+EQUALS.addEventListener("click", function (e) {
+    calculateOne()
+    // handle more than 2 operands
+    if (operands.length > 2) {
+        calculateMultiple(operands, operators)
+    }
+})
+
+// perform simple calculation with 2 operands only
+const calculateOne = () => {
+    operands.push(SCREEN.textContent)
+    updateScreen(operate(operators[0], operands[0], operands[1]))
+}
+
+// perform string of calculations with multiple operators and operands
+const calculateMultiple = (numArray, symbolArray) => {
+    let result = 0
+    // iterate over operands
+    for (let i = 0; i < numArray.length; i++) {
+        // iterate over operators
+        for (let k = 0; k < symbolArray.length; k++) {
+            let previous = numArray[0]
+            let next = numArray[1]
+            let operator = symbolArray[0]
+
+            result = operate(operator, previous, next)
+            // remove the operator that was just used
+            symbolArray = symbolArray.slice(1)
+            // remove the operands that were just used
+            numArray = numArray.slice(2)
+            // put the previous result as the first operand for a new operation
+            numArray.unshift(result)
+        }
+    }
+    updateScreen(result)
+}
+
+const operate = (op, a, b) => {
+    a = parseFloat(a)
+    b = parseFloat(b)
+    switch (op) {
+        case "+":
+            return a + b;
+            break;
+        case "-":
+            return a - b;
+            break;
+        case "*":
+            return a * b;
+            break;
+        case "/":
+            return a / b;
+            break;
+    }
+}
